3089aa1d9c2f82369228eb0a1b290f8bdfebbb78
Balázs Keresztury <b@belidzs.hu>
2023-11-24 20:21:16 +0100

    Bump to 3.2.3

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/gilbertchen/duplicacy/releases/download/v3.1.0/duplicacy_win_i386_3.1.0.exe'
-$url64      = 'https://github.com/gilbertchen/duplicacy/releases/download/v3.1.0/duplicacy_win_x64_3.1.0.exe'
-$checksum      = 'fcaf2c9f5dd32fbbeb449b877e01019dcec319963640ae31eab973654650c278'
+$url        = 'https://github.com/gilbertchen/duplicacy/releases/download/v3.2.3/duplicacy_win_i386_3.2.3.exe'
+$url64      = 'https://github.com/gilbertchen/duplicacy/releases/download/v3.2.3/duplicacy_win_x64_3.2.3.exe'
+$checksum      = 'b57150c84c95bc0ff176cafde36af7b08a60679526fb7a18aa3fd505b28a3819'
 $checksumType  = 'sha256'
-$checksum64    = '196aa7f2f3ef70a83ec659142c6fcce69301e1bdb415faf804a67183a638e828'
+$checksum64    = '2c5e2fd0c9bc74e8538cbe81fc53f09447b68a95c2b4324ed43928deb99a25af'
 $checksumType64= 'sha256'
 
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $toolsDir\'duplicacy.exe' -Url $url -Url64bit $url64 -Checksum $checksum -ChecksumType $checksumType -Checksum64 $checksum64 -ChecksumType64 $checksumType64
